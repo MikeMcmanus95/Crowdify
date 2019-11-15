3736967a866ef047c9a7ab4d718028d38953ab08
Michael Mcmanus <mikemcmanus95@gmail.com>
2019-11-15 17:31:42 -0500

    Got spotify API working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Spotify from 'spotify-web-api-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+const spotifyWebApi = new Spotify();
+
+class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    this.state = {
+      loggedIn: params.access_token ? true : false,
+      nowPlaying: {
+        name: 'None',
+        image: '',
+      },
+    };
+    if (params.access_token) {
+      spotifyWebApi.setAccessToken(params.access_token);
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  async getNowPlaying() {
+    const response = await spotifyWebApi.getMyCurrentPlaybackState();
+    console.log(response);
+    this.setState({
+      nowPlaying: {
+        name: response.item.name,
+        image: response.item.album.images[0].url,
+      },
+    });
+  }
+  render() {
+    const login = (
+      <a className="waves-effect waves-light btn" href="http://localhost:8888">
+        Login with Spotify
+      </a>
+    );
+    const player = (
+      <div className="App">
+        <div>Now Playing: {this.state.nowPlaying.name}</div>
+        <div>
+          <img
+            src={this.state.nowPlaying.image}
+            style={{ width: 500, height: 500 }}
+          ></img>
+        </div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="waves-effect waves-light btn"
+          onClick={() => this.getNowPlaying()}
         >
-          Learn React
+          Check Now Playing
         </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+    if (this.state.loggedIn === false) {
+      return login;
+    }
+    return player;
+  }
 }
 
 export default App;
