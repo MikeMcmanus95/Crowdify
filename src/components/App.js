375fee0350f3f5c064dc069ce873fc211f16f59d
Michael Mcmanus <mikemcmanus95@gmail.com>
2019-11-17 02:25:53 -0500

    refactor(component): ensure app component is clear of api calls Remove addToPlaylist function

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import Spotify from 'spotify-web-api-js';
-import Search from './Search';
-import { Login } from './Login';
 import Routes from '../routes';
 import Navbar from './Navbar';
 
-const spotifyWebApi = new Spotify();
-
 class App extends Component {
-  async addToPlaylist() {
-    const playlistId = this.state.playlist.id;
-    const trackUri = this.state.nowPlaying.uri;
-    const response = await spotifyWebApi.addTracksToPlaylist(playlistId, [
-      trackUri,
-    ]);
-    if (response) {
-      console.log('success?');
-      console.log(response);
-    }
-  }
   render() {
     return (
       <div>
